9b3379f24cef967db55b2ea60a8477b62b5f0735
Rongier Damien <dam@MacBook-Pro-de-Rongier.local>
2025-09-10 10:37:33 +0200

    ex02

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -1,15 +1,42 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-#include <vector>
+#include <stack>
+#include <iterator>
 #include <iostream>
 
-class MutantStack {
-	private:
+template<typename T>
+class MutantStack : public std::stack<T> {
+public:
 
-
-	public:
-
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+    typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+    
+    // Constructeurs (utilise ceux de std::stack par défaut)
+    MutantStack() : std::stack<T>() {}
+    MutantStack(const MutantStack& src) : std::stack<T>(src) {}
+    MutantStack& operator=(const MutantStack& src) {
+        if (this != &src) {
+            std::stack<T>::operator=(src);
+        }
+        return *this;
+    }
+    ~MutantStack() {}
+    
+    // Méthodes d'itérateurs - accès au conteneur sous-jacent
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
+    
+    const_iterator begin() const { return this->c.begin(); }
+    const_iterator end() const { return this->c.end(); }
+    
+    reverse_iterator rbegin() { return this->c.rbegin(); }
+    reverse_iterator rend() { return this->c.rend(); }
+    
+    const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+    const_reverse_iterator rend() const { return this->c.rend(); }
 };
 
 #endif
