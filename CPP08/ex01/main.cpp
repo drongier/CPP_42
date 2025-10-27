1097273cd8415e2b856e5eec57a388d02513c140
Drongier <rongier.damien@gmail.com>
2025-10-27 18:14:50 +0100

    add try and catch to protec and better number to add in vector

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -8,20 +8,24 @@ sp.addNumber(1);
 sp.addNumber(3);
 sp.addNumber(10);
 
-std::cout << sp.shortestSpan() << std::endl;
-std::cout << sp.longestSpan() << std::endl;
+std::cout << "shortestSpan : " << sp.shortestSpan() << std::endl;
+std::cout << "longestSpan : " << sp.longestSpan() << std::endl;
 
 std::vector<int> bigNumbers;
-for (int i = 0; i < 100; i++) {
-    bigNumbers.push_back(i);
-    std::cout << bigNumbers[i];
+for (int i = 0; i < 1001; i++) {
+    bigNumbers.push_back(i * 7 + (i % 13));
+    std::cout << "["<< bigNumbers[i] << "]";
 }
 
-Span bigSpan(100);
-bigSpan.addRange(bigNumbers.begin(), bigNumbers.end());
-
-std::cout << "shortestSpan : " << bigSpan.shortestSpan() << std::endl;
-std::cout << "longestSpan : " << bigSpan.longestSpan() << std::endl;
+Span bigSpan(1001);
+try {
+    bigSpan.addRange(bigNumbers.begin(), bigNumbers.end());
+    std::cout << std::endl << "shortestSpan : " << bigSpan.shortestSpan() << std::endl;
+    std::cout << "longestSpan : " << bigSpan.longestSpan() << std::endl;
+} catch (const Span::SpanFullException& e) {
+    std::cout << std::endl << "Erreur : " << e.what() << std::endl;
+}
+std::cout << std::endl;
 
 return 0;
 
