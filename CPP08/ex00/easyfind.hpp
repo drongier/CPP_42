3c818e4cb444d33f743d83e6bd5c764a7ffd0279
Drongier <rongier.damien@gmail.com>
2025-10-27 17:43:57 +0100

    Add std::distance to find position

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drongier <drongier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 17:43:21 by drongier          #+#    #+#             */
+/*   Updated: 2025/10/27 17:43:22 by drongier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
@@ -7,7 +19,7 @@ template<typename T>
 typename T::iterator easyfind(T& a, int value) {
 	typename T::iterator find_value = std::find(a.begin(), a.end(), value);
 	if (find_value != a.end())
-	    std::cout << "Found " << value << " at position " << (find_value - a.begin()) << '\n';
+	    std::cout << "Found " << value << " at position " << std::distance(a.begin(), find_value) << '\n';
 	else
 		std::cout << "Value not found !" << std::endl;
 	return find_value; 
