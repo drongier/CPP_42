854a353ef2e299bcf883d39b95b03df89d5ac403
Drongier <rongier.damien@gmail.com>
2025-10-27 16:59:23 +0100

    last fix

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -1,32 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drongier <drongier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 12:55:42 by drongier          #+#    #+#             */
+/*   Updated: 2025/10/27 12:55:43 by drongier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
 #include <exception>
 #include <iostream>
 
-
 template<class T>
 class Array {
 private:
-    unsigned int    _size;
-    T*              _array;
+	unsigned int	_size;
+	T*				_array;
 
-    class OutOfBoundException: public std::exception {
+	class OutOfBoundException: public std::exception {
 	public:
 		const char *what () const throw();
 	};
 
 public:
-    Array();
-    Array(unsigned int n);
-    Array( const Array& src);
-    Array& operator=(const Array& src);
-    ~Array();
+	Array();
+	Array(unsigned int n);
+	Array( const Array& src);
+	Array& operator=(const Array& src);
+	~Array();
 
-    T&			operator[](const unsigned int i);
+	T&			operator[](const unsigned int i);
 	const T&	operator[](const unsigned int i) const;
-
-    unsigned int	getSize(void) const;
+	unsigned int	getSize(void) const;
 };
 
 template <class T>
