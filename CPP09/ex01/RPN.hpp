1d715e5033c9f0f2178cea7ce539646f71deac81
Drongier <rongier.damien@gmail.com>
2025-11-10 15:14:12 +0100

    fix error throw and clean code

--- CPP09/ex01/RPN.hpp
+++ CPP09/ex01/RPN.hpp
@@ -7,9 +7,6 @@
 class RPN {
 	private: 
 		std::stack<int> _stack;
-		bool isOperation(char c);
-		int performOperation(int a, int b, char op);
-
 	public:
 		RPN();
 		int calculate(const std::string& expression);
--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -1,13 +1,19 @@
 #include "RPN.hpp"
 
 int main(int ac, char **av) {
+	try {	
+		if (ac != 2) { 
+			std::cerr << "Error : input" << std::endl;
+			return (1);
+		}
 
-	if (ac != 2)
-		throw "Error : input";
-
-	RPN rpn;
-	int result = rpn.calculate(av[1]);
-	std::cout << result << std::endl;
-
-	return(0); 
+		RPN rpn;
+		int result = rpn.calculate(av[1]);
+		std::cout << result << std::endl;
+		
+	} catch (const std::runtime_error& e) {
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	return (0);
 }
